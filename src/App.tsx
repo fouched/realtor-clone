2a6eea86f5b855845dfcb0d5c12794baacb4582b
Fouche du Preez <fouche@limehouse.co.za>
2022-10-11 21:45:33 +0200

    added pages and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import ForgotPassword from "./pages/ForgotPassword";
+import Home from "./pages/Home";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
-function App() {
-  return (
-    <>
-      <h1>Hello World</h1>
-    </>
-  );
+
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home/>} />
+                <Route path="/forgot-password" element={<ForgotPassword/>} />
+                <Route path="/offers" element={<Offers/>} />
+                <Route path="/profile" element={<Profile/>} />
+                <Route path="/sign-in" element={<SignIn/>} />
+                <Route path="/sign-up" element={<SignUp/>} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
